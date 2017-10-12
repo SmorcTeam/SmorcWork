1b1f1711ab18f02a92ebf1993c7e6eb4ab865665
Ladres <Sanyokdonetruuu@yandex.ru>
2017-10-12 14:53:22 +0400

    #8_New_Version_Slider

--- js/jquery.paralax.map_university.js
+++ js/jquery.paralax.map_university.js
@@ -14,3 +14,32 @@ $(function () {
         });
     });
 });
+
+
+var slideIndex = 1;
+showSlides(slideIndex).
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n){
+    showSlides(slideIndex = n)
+}
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    
+    if(n > slides.length){
+       slideIndex=slides.length 
+    }
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace("active","");
+    }
+    slides[slideIndex-1].slide.display = "block";
+    dots[slideIndex-1].className += "active";
+}
